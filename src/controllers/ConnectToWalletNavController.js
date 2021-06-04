2db27da9a1c29bbecb006cdd98417f304baa2f9d
King Kong <king-kong@gmail.com>
2021-06-03 23:42:36 -0700

    Auto Deploy

--- src/controllers/ConnectToWalletNavController.js
+++ src/controllers/ConnectToWalletNavController.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ConnectToWalletNavView from '../views/ConnectToWalletNavView';
 import { connectWallet } from '../constants';
 
+class ConnectToWalletNavController extends React.Component {
+
+
+    
+}
 
 export default (props) => (
     <div>
