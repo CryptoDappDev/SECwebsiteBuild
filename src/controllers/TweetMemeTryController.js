94e73816a0c5b9b8054eb177a01fdaf2c987b28d
CryptoDappDev <joegmitt@gmail.com>
2021-06-10 20:22:13 -0700

    Another One

--- src/controllers/TweetMemeTryController.js
+++ src/controllers/TweetMemeTryController.js
@@ -1,7 +1,6 @@
 import { render } from '@testing-library/react'
 import React from 'react'
 import {TwitterTweetEmbed} from 'react-twitter-embed';
-import TweetMemeView from '../views/TweetMemeView';
 import TweetSecController from './TweetSecController';
 
 class TweetMemeController extends React.Component {
