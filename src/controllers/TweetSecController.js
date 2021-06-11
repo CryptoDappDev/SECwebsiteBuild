8385399559daca3ae4532d94fcc8a3d1ee9b5fb9
CryptoDappDev <joegmitt@gmail.com>
2021-06-10 21:46:33 -0700

    Another One

--- src/controllers/TweetSecController.js
+++ src/controllers/TweetSecController.js
@@ -5,7 +5,7 @@ class TweetSecController extends React.Component {
 
     render() {
         return (
-            <div class="justify-content-center" >
+            <div class="justify-content-center p-2" >
                 <TwitterTweetEmbed tweetId='1278764736876773383' options={{
                     align: 'center'
                     //maxWidth: 500,
