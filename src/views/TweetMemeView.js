a7786f3c27cdc52e2552b944360416a0a0c4594b
CryptoDappDev <joegmitt@gmail.com>
2021-06-10 20:01:47 -0700

    appfairy: Migrate

--- src/views/TweetMemeView.js
+++ src/views/TweetMemeView.js
@@ -63,13 +63,11 @@ class TweetMemeView extends React.Component {
           @media (max-width:767px) and (min-width:480px) {html.w-mod-js:not(.w-mod-ix) [data-w-id=\"b765d888-a64f-7ef3-2f38-59e8d91c9d50\"] {display:none;opacity:0;}html.w-mod-js:not(.w-mod-ix) [data-w-id=\"ad1fe3e8-a35f-7165-8f77-b8a70802d25e\"] {display:none;opacity:0;}}@media (max-width:479px) {html.w-mod-js:not(.w-mod-ix) [data-w-id=\"b765d888-a64f-7ef3-2f38-59e8d91c9d50\"] {display:none;opacity:0;}html.w-mod-js:not(.w-mod-ix) [data-w-id=\"ad1fe3e8-a35f-7165-8f77-b8a70802d25e\"] {display:none;opacity:0;}}
         ` }} />
         <span className="af-view">
-          <div id="elon-meme-tweet" className="af-class-div-block-13">
-            <div className="af-class-div-block-44">
-              <div className="af-class-html-embed-7 w-embed w-script">
-                <blockquote className="af-class-twitter-tweet">
-                  <p lang="en" dir="ltr">Who controls the memes,<br />controls the Universe</p>— Elon Musk (@elonmusk) <a href="https://twitter.com/elonmusk/status/1276418907968925696?ref_src=twsrc%5Etfw">June 26, 2020</a>
-                </blockquote>
-              </div>
+          <div className="af-class-div-block-44">
+            <div className="af-class-html-embed-7 w-embed w-script">
+              <blockquote className="af-class-twitter-tweet">
+                <p lang="en" dir="ltr">Who controls the memes,<br />controls the Universe</p>— Elon Musk (@elonmusk) <a href="https://twitter.com/elonmusk/status/1276418907968925696?ref_src=twsrc%5Etfw">June 26, 2020</a>
+              </blockquote>
             </div>
           </div>
         </span>
