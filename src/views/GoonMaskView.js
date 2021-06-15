d07af2f5a8a760193108a910be3a1d96988433f8
CryptoDappDev <joegmitt@gmail.com>
2021-06-15 15:14:53 -0700

    Uh

--- src/views/GoonMaskView.js
+++ src/views/GoonMaskView.js
@@ -59,8 +59,6 @@ class GoonmaskView extends React.Component {
           @import url(C:\\css\\normalize.css);
           @import url(C:\\css\\components.css);
           @import url(C:\\css\\suck-elons-cock.css);
-
-          @media (max-width:767px) and (min-width:480px) {html.w-mod-js:not(.w-mod-ix) [data-w-id=\"b765d888-a64f-7ef3-2f38-59e8d91c9d50\"] {display:none;opacity:0;}html.w-mod-js:not(.w-mod-ix) [data-w-id=\"ad1fe3e8-a35f-7165-8f77-b8a70802d25e\"] {display:none;opacity:0;}}@media (max-width:479px) {html.w-mod-js:not(.w-mod-ix) [data-w-id=\"b765d888-a64f-7ef3-2f38-59e8d91c9d50\"] {display:none;opacity:0;}html.w-mod-js:not(.w-mod-ix) [data-w-id=\"ad1fe3e8-a35f-7165-8f77-b8a70802d25e\"] {display:none;opacity:0;}}
         ` }} />
         <span className="af-view">
           <div id="goon-nft-2" className="af-class-purchase-button-for-nfts">
