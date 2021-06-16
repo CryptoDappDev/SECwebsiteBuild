13eca4ebff6c172a696af77870110ee919d6d054
CryptoDappDev <joegmitt@gmail.com>
2021-06-16 14:12:50 -0700

    appfairy: Migrate

---
+++ src/views/GoonTrippyView.js
@@ -0,0 +1,75 @@
+/* eslint-disable */
+
+import React from 'react'
+import { createScope, map, transformProxies } from './helpers'
+
+const scripts = [
+
+]
+
+let Controller
+
+class GoonTrippyView extends React.Component {
+  static get Controller() {
+    if (Controller) return Controller
+
+    try {
+      Controller = require('../controllers/GoonTrippyController')
+      Controller = Controller.default || Controller
+
+      return Controller
+    }
+    catch (e) {
+      if (e.code == 'MODULE_NOT_FOUND') {
+        Controller = GoonTrippyView
+
+        return Controller
+      }
+
+      throw e
+    }
+  }
+
+  componentDidMount() {
+    /* View has no WebFlow data attributes */
+
+    scripts.concat(null).reduce((active, next) => Promise.resolve(active).then((active) => {
+      const loading = active.loading.then((script) => {
+        new Function(`
+          with (this) {
+            eval(arguments[0])
+          }
+        `).call(window, script)
+
+        return next
+      })
+
+      return active.isAsync ? next : loading
+    }))
+  }
+
+  render() {
+    const proxies = GoonTrippyView.Controller !== GoonTrippyView ? transformProxies(this.props.children) : {
+
+    }
+
+    return (
+      <span>
+        <style dangerouslySetInnerHTML={{ __html: `
+          @import url(C:\\css\\normalize.css);
+          @import url(C:\\css\\components.css);
+          @import url(C:\\css\\suck-elons-cock.css);
+        ` }} />
+        <span className="af-view">
+          <div id="goon-nft-2" className="af-class-purchase-button-for-nfts">
+            <a href="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/31673350562246474401696226698567993468440352420607247670176787854364680126533/" target="_blank" className="af-class-buy-button af-class-goon w-button">Purchase</a>
+          </div>
+        </span>
+      </span>
+    )
+  }
+}
+
+export default GoonTrippyView
+
+/* eslint-enable */
