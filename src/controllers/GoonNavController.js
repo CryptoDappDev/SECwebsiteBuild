9229cfebddbd36eec47d20a6835f06ccd7aebfdf
CryptoDappDev <joegmitt@gmail.com>
2021-06-19 19:37:21 -0700

    uh

---
+++ src/controllers/GoonNavController.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import GoonNavView from '../views/GoonNavView';
+
+
+function GoonNavController() {
+
+
+    return (
+        <GoonNavView/>
+    )
+}
+
+export default (props) => (
+
+    <GoonNavController/>
+)
