1d7fa8ae09a9ab1f7a2a31e645414acfd6151b6e
CryptoDappDev <joegmitt@gmail.com>
2021-06-22 18:17:35 -0700

    uh

---
+++ src/controllers/MemeCoinListController.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import IndexView from '../views/IndexView'
+import { MetamaskStateProvider } from 'use-metamask';
+import MemeCoinListView from '../views/MemeCoinListView';
+
+function MemeCoinListController() {
+
+  return (
+    <div>Hello!</div>
+  )
+}
+
+export default (props) => (
+  <MemeCoinListController> 
+  
+  </MemeCoinListController>
+)
