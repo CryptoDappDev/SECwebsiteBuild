0dddb8f6d2ac96d8517592bbe96f8cab2e163ee6
CryptoDappDev <joegmitt@gmail.com>
2021-06-22 18:26:42 -0700

    uh

--- src/controllers/MemeCoinListController.js
+++ src/controllers/MemeCoinListController.js
@@ -6,7 +6,27 @@ import MemeCoinListView from '../views/MemeCoinListView';
 function MemeCoinListController() {
 
   return (
-    <div>Hello!</div>
+    <span>
+    <style dangerouslySetInnerHTML={{ __html: `
+      @import url(C:\\css\\normalize.css);
+      @import url(C:\\css\\components.css);
+      @import url(C:\\css\\suck-elons-cock.css);
+    ` }} />
+    <span className="af-view">
+      <div data-collapse="all" data-animation="default" data-duration={400} role="banner" className="af-class-coinincubator w-nav">
+        <div className="af-class-container-5 w-container">
+          <div className="af-class-text-block-31">Coin Incubator<span className="af-class-text-span-14">™</span></div>
+          <nav role="navigation" className="af-class-nav-menu-2 w-nav-menu">
+            <a href="SEC" className="af-class-seclink w-nav-link">$SEC</a>
+            <a href="GoonRich" className="af-class-goonlink w-nav-link">$GOON</a>
+          </nav>
+          <div className="af-class-menu-button-5 w-nav-button">
+            <div className="af-class-icon w-icon-dropdown-toggle" />
+          </div>
+        </div>
+      </div>
+    </span>
+  </span>
   )
 }
 
